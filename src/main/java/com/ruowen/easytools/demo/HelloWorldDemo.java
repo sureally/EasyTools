eb43c5efbb4a3e970301172abf51322516315326
舒文君 <shu_wenjun@qq.com>
2021-03-07 14:54:46 +0800

    hello world demo

---
+++ src/main/java/com/ruowen/easytools/demo/HelloWorldDemo.java
@@ -0,0 +1,30 @@
+package com.ruowen.easytools.demo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
+
+/**
+ * hello world demo
+ */
+public class HelloWorldDemo extends AnAction {
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    // 获取当前在操作的工程上下文
+    Project project = e.getData(PlatformDataKeys.PROJECT);
+
+    // 获取当前操作的类文件
+    PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
+    // 获取当前类文件的路径
+    String classPath = psiFile.getVirtualFile().getPath();
+    String title = "Hello World!";
+
+    // 显示对话框
+    Messages.showMessageDialog(project, classPath, title, Messages.getInformationIcon());
+  }
+}
